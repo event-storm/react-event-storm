222e38cd59574075010d9c0f1ca0fb4c5b935a2e
Artur Gevorgyan <artur93gev@gmail.com>
2021-01-25 12:40:21 +0300

    fix(dist): remove dist coming from action release

--- dist/index.js
+++
@@ -1,2 +0,0 @@
-import{useState,useEffect}from"react";const useModels=(...a)=>{const[b,c]=useState();return useEffect(()=>{const b=a.map(a=>a.subscribe(()=>c([])));return()=>b.map(a=>a())},[]),a.map(a=>a.getState())};export{useModels};
-//# sourceMappingURL=index.js.map
