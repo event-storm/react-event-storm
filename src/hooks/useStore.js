9480fb44732e2b2e1fd38756349601f6ca4d75bb
Artur Gevorgyan <artur93gev@gmail.com>
2021-03-18 23:40:36 +0300

    fix(usestore): proxied from wrong object

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -3,7 +3,7 @@ import { useMemo } from 'react';
 import { createProxy } from './utils';
 
 const useStore = store => {
-  const proxy = useMemo(() => createProxy(store, {
+  const proxy = useMemo(() => createProxy(store.models, {
     getter: key => store.models[key].getState(),
   }), [store]);
   return proxy;
