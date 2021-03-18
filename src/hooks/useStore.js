acfed4bc8ecde6205eec77712a88f6e184dd9cf3
Artur Gevorgyan <artur93gev@gmail.com>
2021-03-18 23:47:19 +0300

    fix(usestore): udpate was not perfromed

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,11 +1,15 @@
 import { useMemo } from 'react';
 
 import { createProxy } from './utils';
+import useSubscribe from './useSubscribe';
 
 const useStore = store => {
   const proxy = useMemo(() => createProxy(store.models, {
     getter: key => store.models[key].getState(),
   }), [store]);
+
+  useSubscribe(Object.values(store.models));
+
   return proxy;
 }
 
---
+++ src/hooks/useSubscribe.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+
+const useSubscribe = models => {
+  const [_, forceUpdate] = useState();
+
+  useEffect(() => {
+    const subscriptions = models.map(model => model.subscribe(() => forceUpdate([])));
+    return () => subscriptions.map(unsubscribe => unsubscribe());
+  }, []);
+}
+
+export default useSubscribe;
