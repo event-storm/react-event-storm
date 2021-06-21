e585f79317e5a6a9a4b4b1d918146c882881afb9
Artur Gevorgyan <artur93gev@gmail.com>
2021-06-22 01:31:40 +0400

    feat(types): imrpove typescript support

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,7 @@
-import { IModel, IStore } from 'event-storm';
+import { IModel, IStore, IStoreState } from 'event-storm';
 
 export function useModels(...models: IModel<any>[]): any[];
 
-export function useStore<T>(store: IStore<T>): T;
+export function useStore<T>(store: IStore<T>): IStoreState<T>;
 
-export function usePublish<T>(store: IStore<Partial<T>>): IStore['publish'];
+export function usePublish<T>(segment: IStore<Partial<T>>): IStore['publish'];
