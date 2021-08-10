552eea06d480006d8252c1368ff3c9209b7dab14
Artur Gevorgyan <artur93gev@gmail.com>
2021-08-10 23:28:24 +0300

    feat(usestore): adding a control property for store subscription
    Now its possible to control the subcription of the store for any component. Active, passive and
    condition-based

--- src/hooks/useStoreExperimental.js
+++ src/hooks/useStoreExperimental.js
@@ -1,16 +1,18 @@
 import { useMemo, useRef, useEffect } from 'react';
-import { EVENT_STORM_INTERNALS } from 'event-storm';
 
 import { createProxy } from './utils';
 import useForceUpdate from './useForceUpdate';
 
-const useStore = store => {
+const useStore = (store, options = { active: true }) => {
   const modelRefs = useRef({});
   const forceUpdate = useForceUpdate();
 
   const proxy = useMemo(() => createProxy(store.models, {
     getter: key => {
-      if (EVENT_STORM_INTERNALS.subscriptionMode.getState() && !modelRefs.current[key]) {
+      if (!options.active && modelRefs.current[key]) {
+        modelRefs.current[key]();
+      }
+      if (!modelRefs.current[key] && options.active) {
         modelRefs.current[key] = store.models[key].subscribe(forceUpdate);
       }
       return store.models[key].getState();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as useStore } from './hooks/useStore';
 export { default as useModels } from './hooks/useModels';
 export { default as usePublish } from './hooks/usePublish';
+export { default as useStoreExperimental } from './hooks/useStoreExperimental';
