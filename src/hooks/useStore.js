2921f686d0656a5de11a69869847bb78b74239b1
Artur Gevorgyan <artur93gev@gmail.com>
2021-08-15 16:42:52 +0200

    refactor(usestore): remove unregisteres models

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,20 +1,14 @@
 import { useMemo, useRef, useEffect } from 'react';
 
 import { createProxy } from './utils';
-import useUpdate from './useUpdate';
 import useForceUpdate from './useForceUpdate';
 
 const useStore = (store, options = { active: true }) => {
   const modelRefs = useRef({});
-  const notRegisteredRefs = useRef([]);
   const forceUpdate = useForceUpdate();
 
   const proxy = useMemo(() => createProxy(store.models, {
     getter: key => {
-      if (!store.models[key]) {
-        notRegisteredRefs.current.push(key);
-        return;
-      }
       if (!options.active && modelRefs.current[key]) {
         modelRefs.current[key]();
       }
@@ -24,8 +18,6 @@ const useStore = (store, options = { active: true }) => {
       return store.models[key].getState();
     },
   }), [store.models.length]);
-
-  useUpdate(forceUpdate, [store.models.length]);
 
   useEffect(() => () => Object.values(modelRefs.current).forEach(unsubscribe => unsubscribe()), []);
 
