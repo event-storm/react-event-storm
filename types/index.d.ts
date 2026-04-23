cb01bd353c50807a102c3e81477124b5f993af42
Artur Gevorgyan <artur93gev@pgmail.com>
2026-04-23 20:04:04 +0400

    fix(types): storm FragmentSubscribe must return correct types, when picking
    the type must be derived from the IStormState's generic, to be able infer lately

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,13 +6,13 @@ export interface ISubscriptionOptions {
 
 export function useModels<T extends IModelConfiguration>(models: IModel<any, T>[], options?: ISubscriptionOptions): any[];
 
-export type FragmentSubscribe = (fragment: any) => any;
-export type SelectFragment<T, K = any> = (
+export type FragmentSubscribe = <Fragment>(fragment: Fragment) => Fragment;
+export type SelectFragment<T, K> = (
   state: IStormState<T>,
   subscribe: FragmentSubscribe,
 ) => K;
 
-export function useStorm<T, K = any>(
+export function useStorm<T, K = IStormState<T>>(
   storm: IStorm<T>,
   selectToFragment?: SelectFragment<T, K>,
   options?: ISubscriptionOptions,
